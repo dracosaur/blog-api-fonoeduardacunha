436abc942707c66339405c890ea480ccefc8d9e7
Pietro Saraiva <pietro.saraiva@gmail.com>
2022-06-16 16:16:07 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'db19d32453add06573d5c4afe5064bf0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'deb80565-9223-4813-86fc-b9d7af4cfa4f'
+};
